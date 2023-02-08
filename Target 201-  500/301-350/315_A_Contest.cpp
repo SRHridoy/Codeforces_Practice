261336bc04c221b6351ad025dd60e737a64fd086
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-02-08 14:27:32 +0600

    Completed my daily code - forces target...

---
+++ Target 201-  500/301-350/315_A_Contest.cpp
@@ -0,0 +1,43 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define all(v)            v.begin(),v.end()
+#define fo(i, n)          for(int i = 0; i < n; i++)
+#define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+using namespace std;
+
+void solve(){
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    int misha = max((3*a)/10,a- ((a*c)/250));
+    int vasya = max((3*b)/10,b- ((b*d)/250));
+    if(misha>vasya) cout << "Misha" << endl;
+    else if(vasya>misha) cout << "Vasya" << endl;
+    else cout << "Tie" << endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    t = 1;
+    //int cs = 1;
+    //cin >> t;
+    while (t--){
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
