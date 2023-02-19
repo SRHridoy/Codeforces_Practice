ba35d292501208c51b4c1a016ca5ca360d823c6c
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-02-19 08:20:59 +0600

    Completed my daily code forces target...

---
+++ Target 201-  500/301-350/332_B_Limit.cpp
@@ -0,0 +1,53 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define all(v)            v.begin(),v.end()
+#define fo(i, n)          for(int i = 0; i < n; i++)
+#define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+using namespace std;
+
+void solve(){
+    int n, m; cin >> n >> m;
+    vector<int>Px(n+2);
+    fo(i,n+1) cin >> Px[i];
+    vector<int>Qx(m+1);
+    fo(i,m+1) cin >> Qx[i];
+    if(n>m){
+        if(Px[0]*Qx[0] > 0) cout << "Infinity" << endl;
+        else cout << "-Infinity" << endl;
+        return;
+    }else if(n < m){
+        cout << "0/1" << endl;
+        return;
+    }else{
+        if(Px[0]*Qx[0] < 0) cout << "-";
+        cout << abs(Px[0])/__gcd(abs(Px[0]), abs(Qx[0])) << "/" <<abs(Qx[0])/__gcd(abs(Px[0]), abs(Qx[0]));
+    }
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    t = 1;
+    //int cs = 1;
+    //cin >> t;
+    while (t--){
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
