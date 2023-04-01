fc7f8c0fae2474980b29ca64a55dfe9cafb2c303
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-04-01 21:26:22 +0600

    add a program...

---
+++ CP Contests/02.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+    int n; cin >> n;
+    if(n>=1 and n<=9){
+        cout << "YES" << endl;
+    }else if( n== 25){
+        cout << "YES" << endl;
+    }else if (n==10){
+        cout << "NO" << endl;
+    }
+    
+}
