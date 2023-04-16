b266c8135a846d9f021583a9172238b1f7968718
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-04-16 13:38:30 +0600

    Started CP SHEET...

---
+++ CP_Sheet/01_Implementation_Problems/A_Bit.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/282/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    while(n--){
+        string x;
+        cin >> x;
+        if(x=="X++" or x == "++X")
+            ans++;
+        else
+            ans--;
+    }
+    cout << ans << endl;
+}
